4c92059eb639b5a57129a5fa116ca4823f7c020e
AVS1508 <aditya.11.present@gmail.com>
2023-05-10 02:00:27 -0400

    Modified files to create stereoscopic image pairs from 20 different angles

--- create_viewpoints.py
+++ create_viewpoints.py
@@ -6,21 +6,22 @@ def get_stereoscopic_views(azimuth, elevation, distance, eye_distance):
     right_azimuth = azimuth + math.radians(eye_distance / 2)
     
     # Convert back to Cartesian coordinates
-    left_x = distance * math.cos(left_azimuth) * math.cos(elevation)
-    left_y = distance * math.sin(left_azimuth) * math.cos(elevation)
-    left_z = distance * math.sin(elevation)
+    # left_x = distance * math.cos(left_azimuth) * math.cos(elevation)
+    # left_y = distance * math.sin(left_azimuth) * math.cos(elevation)
+    # left_z = distance * math.sin(elevation)
     
-    right_x = distance * math.cos(right_azimuth) * math.cos(elevation)
-    right_y = distance * math.sin(right_azimuth) * math.cos(elevation)
-    right_z = distance * math.sin(elevation)
+    # right_x = distance * math.cos(right_azimuth) * math.cos(elevation)
+    # right_y = distance * math.sin(right_azimuth) * math.cos(elevation)
+    # right_z = distance * math.sin(elevation)
     
-    return [(left_x, left_y, left_z), (right_x, right_y, right_z)]
+    # return [(left_x, left_y, left_z), (right_x, right_y, right_z)]
+    return [left_azimuth, elevation, 0.0, distance], [right_azimuth, elevation, 0.0, distance]
 
 if __name__ == '__main__':
     phi = (1 + math.sqrt(5)) / 2. # golden_ratio
     circumradius = math.sqrt(3)
     distance = circumradius * 1.2
-    eye_distance = 0.06  # Adjust this value to control the eye separation
+    eye_distance = 1  # Adjust this value to control the eye separation
     
     dodecahedron = [[-1, -1, -1],
                     [ 1, -1, -1],
@@ -55,7 +56,7 @@ if __name__ == '__main__':
         left_view, right_view = get_stereoscopic_views(azimuth, elevation, distance, eye_distance)
         
         # Write the left and right viewpoints to the file
-        view_points.write('%f %f %f %f\n' % (left_view[0], left_view[1], left_view[2], distance))
-        view_points.write('%f %f %f %f\n' % (right_view[0], right_view[1], right_view[2], distance))
+        view_points.write('%f %f %f %f\n' % (left_view[0], left_view[1], left_view[2], left_view[3]))
+        view_points.write('%f %f %f %f\n' % (right_view[0], right_view[1], right_view[2], right_view[3]))
     
     view_points.close()
